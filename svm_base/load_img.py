97981ea67c6e4387061bb842637fd021ed01eefe
Arsaln Cheema <arsalanc.v2@gmail.com>
2018-10-07 11:26:12 +0800

    add image visualization method

--- svm_base/load_img.py
+++ svm_base/load_img.py
@@ -3,7 +3,8 @@ import pydicom
 import numpy as np
 from clint.textui import progress
 from sklearn.model_selection import train_test_split
-
+from scipy.misc import toimage
+from matplotlib import pyplot as plt
 import os
 
 def parse_csv(CSV):
@@ -23,18 +24,24 @@ def load_data_into_memory(DIR, ANNO, ATTRIBUTE):
             img = pydicom.read_file(DIR + image_path).pixel_array
             X.append(img)
         except:
-            print('patient id \'{}\' cannot be found'.format(key))        
-            
+            print('patient id \'{}\' cannot be found'.format(key))
+        count += 1
+
     x, y = np.array(X), np.array(y)
     print('Loaded {} images into memory'.format(len(y)))
     return x, y
 
+def visualize_images(images, n):
+	while n > 0:
+		toimage(images[n]).show()
+		n -= 1
+
 if __name__ == '__main__':
-    
+
     TEST_PERCENT = 0.1
     ATTRIBUTE = 'Target'
-    ANNO = 'dataset/stage_1_train_labels.csv'
-    TRAIN_DIR = 'dataset/sample_data'
+    ANNO = '../dataset/stage_1_train_labels.csv'
+    TRAIN_DIR = '../dataset/sample_data'
 
     print('Loading Train Data')
     X, Y = load_data_into_memory(DIR=TRAIN_DIR, ANNO=ANNO, ATTRIBUTE=ATTRIBUTE)
@@ -42,3 +49,5 @@ if __name__ == '__main__':
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=TEST_PERCENT, random_state = 51)
     print(X_train.shape, 'training data')
     print(X_test.shape, 'test data')
+
+    # visualize_images(X_train, 2)
